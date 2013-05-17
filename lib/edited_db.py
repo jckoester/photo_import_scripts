b9683e9992290a158796e41ab80bf2fab63c148e
Jan Köster <koester_jan@gmx.net>
2013-05-17 19:08:11 +0200

    Additional files for renaming edited raws

---
+++ lib/edited_db.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##Imports
+import os
+import subprocess
+import datetime
+import shutil
+import re
+import sqlite3
+import commonfunc as cf
+#class for manipulating xmp, exif and iptc
+#import pyexiv2
+#exiftool bindings for python (git://github.com/smarnach/pyexiftool.git)
+from pyexiftool_settags import exiftool
+#Global vars
+file_exts=('.tif','.nef','.jpg','.png', '.xmp')
+photo_exts=('.tif','.nef','.jpg','.png')
+verbose=False
+
+
+#Erorr handling
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class PathError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
+
+def init():
+    conn = sqlite3.connect('shuttercount.db')
+    #init
+    c = conn.cursor()
+    # Create table
+    c.execute('''CREATE TABLE shuttercount (timestamp, camera, shuttercount)''')
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+    return
+
+def scan(path):
+    conn = sqlite3.connect('shuttercount.db')
+    c = conn.cursor()
+
+    #check if path exists:
+    if not os.path.exists(path):
+        print "Datei "+path+" existiert nicht."
+    #Get exif information:
+    values = cf.getexifvalue(path, {'DateTimeOriginal', 'Model', 'ShutterCount', 'SerialNumber'} )
+
+    if('EXIF:Model' in values):
+        date=values['EXIF:DateTimeOriginal']
+        date=datetime.datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
+        camera=values['EXIF:Model']
+        
+        if(values['EXIF:Model'][:5]=='NIKON'):
+             camera=values['EXIF:Model'][6:]
+
+        if(values['EXIF:Model'][:5]=='NIKON' and not 'MakerNotes:ShutterCount' in values.keys()	):
+            #Setze ShutterCount auf leeren String:
+            shuttercount=""
+
+        elif(values['EXIF:Model'][:5]=='NIKON'):
+            shuttercount=values['MakerNotes:ShutterCount']
+            camera=values['EXIF:Model'][6:]
+        else:
+            if verbose:
+                print "Für Kamera "+camera+" wurde keine Prozedur definiert."
+            return
+        print date, camera, shuttercount
+        #Check if dataset already in db:
+        t=(date, camera, shuttercount)
+        c.execute("SELECT * FROM shuttercount WHERE timestamp = ? AND camera = ? AND shuttercount = ?", t )
+        if not c.fetchone():
+            if verbose:
+                print "Speichere %s für Datei %s." % (shuttercount, path)
+            c.execute("INSERT INTO shuttercount VALUES (?,?,?)", t)
+            conn.commit()
+            conn.close()
+        else:
+            if verbose:
+                print "%s aus Datei %s schon gespeichert." % (shuttercount, path)
+
+    else:
+        print "Kein Kameramodell in EXIF gespeichert. Datei: "+path+" wurde übersprungen."
+
+    return
+
+def get_shuttercount(date, camera):
+    conn = sqlite3.connect('shuttercount.db')
+    c = conn.cursor()
+
+    #Look in database:
+    t=(date, camera)
+    if verbose:
+        print "Datenbank-Suche nach %s %s" % t
+    c.execute("SELECT shuttercount FROM shuttercount WHERE timestamp = ? AND camera = ? ", t )
+
+    shuttercount=c.fetchone()[0]
+    if not shuttercount:
+        if verbose:
+            print "Keine Daten gefunden für %s %s" % t
+            return False
+    else:
+        if verbose:
+            print "ShutterCount %s zu %s %s gefunden." % (shuttercount, date, camera)
+        return shuttercount
+
+def check(path, *args, **kwargs):
+    #check if path exists:
+    if not os.path.exists(path):
+        print "Datei "+path+" existiert nicht."
+    #Get exif information:
+    values = cf.getexifvalue(path, {'DateTimeOriginal', 'Model', 'ShutterCount', 'SerialNumber'} )
+
+    if('EXIF:Model' in values):
+        date=values['EXIF:DateTimeOriginal']
+        date=datetime.datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
+        camera=values['EXIF:Model']
+        
+        if(values['EXIF:Model'][:5]=='NIKON'):
+             camera=values['EXIF:Model'][6:]
+        return get_shuttercount(date, camera)
+    else:
+        print "Kein Kameramodell in EXIF gespeichert. Datei: "+path+" wurde übersprungen."
+        return
+
---
+++ renameEdited.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#=================DESCRIPTION IS MISSING!========================================#
+
+#System environment
+import sys
+import os
+import datetime
+import subprocess
+import argparse
+import sqlite3
+from lib import commonfunc as cf
+from lib import edited_db as db
+
+##defining the options:
+parser = argparse.ArgumentParser(description='A script for renaming edited raws especially wehre the MakerNotes are missing in the TIF. The scan options scans through a folder for Original files and saves timestamp and shuttercount in a database. The rename option scnas through a folder and renames all files according to the shuttercount values saved in the database.')
+
+parser.add_argument("path", help="Path of the folder containing the images / path to the image")
+
+parser.add_argument("--scan", action="store_true", help="Perform a scan and save to database")
+parser.add_argument("--check", action="store_true", help="Look for data in database")
+
+parser.add_argument("-p", "--prefix", default="", help="2 letter prefix indicating the creator")
+parser.add_argument("-s", "--suffix", default="", help="suffix indicating the type of image. Should be BEA or DMA for edited files (PRN for a printer optimzed file (please use PRN_profilename for profiled images))")
+
+parser.add_argument("-y", "--dontask", action="store_true", help="Perform the command without asking for confirmation.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output.")
+
+#Processing the arguments
+args = parser.parse_args()
+
+
+#Defaults:
+verbose=False
+
+
+#Check for verbose mode
+if args.verbose:
+    verbose=True
+    cf.verbose=True
+    db.verbose=True
+
+#Check source folder
+if not (os.path.exists(args.path)):
+    print "Ordner "+path+" existiert nicht"
+    sys.exit(0)
+
+if not os.path.exists('shuttercount.db'):
+    #Create Database
+    db.init()
+
+if args.scan:
+    procopts=[]
+    procargs={}
+    #If folder
+    if os.path.isdir(args.path):
+        cf.process_folder(args.path, db.scan, procopts, procargs)
+    #If file
+    elif os.path.isfile(args.path):
+        db.scan(args.path, procopts, procargs)
+
+if args.check:
+    procopts=[]
+    procargs={}
+    #If folder
+    if os.path.isdir(args.path):
+        cf.process_folder(args.path, db.check, procopts, procargs)
+    #If file
+    elif os.path.isfile(args.path):
+        db.check(args.path, procopts, procargs)
+
